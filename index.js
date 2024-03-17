aa02f5b8a157ff4fae330a0e60c6ebf961af6f54
rimasultana <rimasultana20225@gmail.com>
2024-03-17 23:19:47 +0600

    class

---
+++ index.js
@@ -0,0 +1,21 @@
+// syntex
+
+class ClassName {
+    constructor(){
+
+    }
+}
+
+
+class Car {
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+}
+
+const myCar = new Car ("Forad", 2038);
+
+const myCar2 = new Car ("Audi", 2019);
+
+console.log(myCar2);
